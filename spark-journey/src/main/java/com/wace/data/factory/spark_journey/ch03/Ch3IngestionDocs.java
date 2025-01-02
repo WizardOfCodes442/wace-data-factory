fafb63c22b144db2eb75f98ebd51deb4194c1bbc
SpaceTech1800 <oliverspace1800@hotmail.com>
2025-01-01 20:29:23 -0800

    second commit

--- spark-journey/src/main/java/com/wace/data/factory/spark_journey/ch03/Ch3IngestionDocs.java
+++ spark-journey/src/main/java/com/wace/data/factory/spark_journey/ch03/Ch3IngestionDocs.java
@@ -1,5 +1,17 @@
 package com.wace.data.factory.spark_journey.ch03;
 
+import org.apache.spark.Partition;
+
 public class Ch3IngestionDocs {
 
+	//Partion are created, and data is assigned to each partion 
+	//automatically based on your infrastructure (number of nodes and
+	// size of the dataset
+	//you can findout how many partition you have with the following code 
+	
+	System.out.println("**** Looking at partitions");
+	Partition[] partitions = df.rdd().partitions();
+	int partitionCount  = partitions.length;
+	System.out.println("Partition count before repartition: " + partitionCount);
+	
 }
